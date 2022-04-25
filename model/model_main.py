8841595f99c532286276059eceeec77640b7fed2
Emre Çalışır <emrecalisir95@gmail.com>
2022-04-25 10:15:17 +0300

    Update model_main.py

--- model/model_main.py
+++ model/model_main.py
@@ -5,12 +5,12 @@ warnings.filterwarnings("ignore")
 
 
 def model_main(normalizer, revisedDict, tfidf_load, svc_load):
-    print("Starting new session...", end="\r", flush=True)
-    print("Getting tweets...", end="\r", flush=True)
+    print("Starting new session...")
+    print("Getting tweets...")
     save_tweets()
-    print("Preprocessing tweets...", end="\r", flush=True)
+    print("Preprocessing tweets...")
     preprocess_df = preprocess(normalizer, revisedDict)
-    print("Prediction...", end="\r", flush=True)
+    print("Prediction...")
     predicted_df = predict(preprocess_df, tfidf_load, svc_load)
 
 
